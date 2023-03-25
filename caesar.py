dd4937b31c811095c5abce4bc1fca321570be493
ulgoon <ulgoon89@gmail.com>
2023-03-25 13:30:49 +0900

    feat: Assign Dictionary shifted to Encode(decode) str

--- caesar.py
+++ caesar.py
@@ -1,6 +1,14 @@
-def caesar() -> str:
-    return 'python work'
+from string import ascii_uppercase as alpha
+
+
+def caesar(to_encrypt: str, k: int, decode=False) -> str:
+    shifted = dict(
+        zip(alpha,
+            alpha[(k, len(alpha)-k)[decode]:]+alpha[:(k, len(alpha)-k)[decode]]
+            )
+    )
+    return shifted
 
 
 if __name__ == '__main__':
-    print(caesar())
+    print(caesar('a', 18))
