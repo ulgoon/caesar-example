248ec20d55f88b37fe71390a4505890ec9ab17ee
ulgoon <ulgoon89@gmail.com>
2023-03-25 13:58:02 +0900

    feat: Add User Interaction

--- caesar.py
+++ caesar.py
@@ -11,6 +11,16 @@ def caesar(to_encrypt: str, k: int, decode=False) -> str:
                    for c in to_encrypt.upper())
 
 
+def get_info() -> tuple:
+    sentence = input('Enter Sentence to encrypt(decrypt): ')
+    cipher_key = int(input('Enter key(1-25): '))
+    is_encrypt = input('(E)ncrypt or (D)ecrypt? ').upper()
+    is_encrypt = False if is_encrypt == 'E' else True
+    return (sentence, cipher_key, is_encrypt)
+
+
 if __name__ == '__main__':
-    print(caesar('Python is awesome language.', 18))
-    print(caesar(caesar('Python is awesome language.', 18), 18, True))
+    sentence, cipher_key, is_encrypt = get_info()
+    print(caesar(sentence, cipher_key, is_encrypt))
+#    print(caesar('Python is awesome language.', 18))
+#    print(caesar(caesar('Python is awesome language.', 18), 18, True))
